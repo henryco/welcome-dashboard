b6e164befbf4720c0981f8a9751b9360f5f1a71f
Henry Domagalski <henrycodev@gmail.com>
2023-08-06 17:50:09 +0200

    wip

--- welcome-dashboard.el
+++ welcome-dashboard.el
@@ -260,7 +260,7 @@
 
 (defun welcome-dashboard--insert-centered (margin text)
   "Insert TEXT at the center of the current line."
-  (let* ((width (- (window-body-width) (* 2 margin) (length text))))
+  (let* ((width (- (window-body-width) (* 2 margin) (length text) 4)))
     (insert (make-string width ?\ ))
     (insert text)))
 
