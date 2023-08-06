26c91b1c44e8f2ff85f42e286b0100c5d44427cd
Henry Domagalski <henrycodev@gmail.com>
2023-08-06 18:45:45 +0200

    wip

--- welcome-dashboard.el
+++ welcome-dashboard.el
@@ -577,7 +577,7 @@ and parse it json and call (as CALLBACK)."
         (welcome-dashboard--insert-recent-files)
         (setq cursor-type nil)
 
-        (welcome-dashboard--insert-centered margin (propertize "[C-number to open file]" 'face 'welcome-dashboard-subtitle-face ))
+        ;; (welcome-dashboard--insert-centered margin (propertize "[C-number to open file]" 'face 'welcome-dashboard-subtitle-face ))
         
         (insert "\n")
         (welcome-dashboard--insert-startup-time)
@@ -586,6 +586,7 @@ and parse it json and call (as CALLBACK)."
 
         (insert "\n\n")
         (welcome-dashboard--insert-centered margin (propertize (format-time-string "%A, %B %d %R") 'face 'welcome-dashboard-time-face))
+        (welcome-dashboard--insert-centered margin (propertize "[C-number to open file]" 'face 'welcome-dashboard-subtitle-face ))
 
         (when (display-graphic-p)
           (insert "\n\n")
